e82ab08d088a9077f3be314e5a25b51e9de6645d
zvdas <rodriguezzude@gmail.com>
2023-01-04 00:03:23 +0530

    task 1 completed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -28,10 +28,11 @@ const ExpenseItem = (props) => {
 
 	return (
 		<tr>
-		<td>{props.name}</td>
-		<td>£{props.cost}</td>
-		<td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-		<td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+			<td>{props.name}</td>
+			<td>£{props.cost}</td>
+			<td><button className='btn btn-outline-none' onClick={event=>increaseAllocation(props.name)}><i className='bi bi-plus-circle-fill fs-4' style={{color: 'green'}}></i></button></td>
+			<td><button className='btn btn-outline-none' onClick={handleDeleteExpense}><i className='bi bi-dash-circle-fill fs-4' style={{color: 'red'}}></i></button></td>
+			<td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
 		</tr>
 	);
 };

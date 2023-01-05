16cf1acd359e65b960660de6ce87bda30a37bbdb
zvdas <rodriguezzude@gmail.com>
2023-01-05 18:13:42 +0530

    budget input trial

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,39 @@
 import React, { useContext } from 'react';
+import { useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-	const { budget } = useContext(AppContext);
+	const { budget, dispatch } = useContext(AppContext);
+
+	const [name, setName] = useState('');
+	const [cost, setCost] = useState('');
+	const [action, setAction] = useState('');
+
+	const setBudget = () => {
+		const expense = {
+			name: name,
+			cost: parseInt(cost),
+		}
+
+		if(action === 'Reduce') {
+			dispatch({
+				type: 'RED_EXPENSE',
+				payload: expense,
+			});
+		} else {
+			dispatch({
+				type: 'ADD_EXPENSE',
+				payload: expense
+			});
+		};
+	};
 
 	return (
 		<div className='alert alert-secondary'>
-			<span>Budget: £{budget}</span>
+			{/* <span>Budget: £{budget}</span> */}
+			<span>Budget: £
+				<input required='required' type='number' value={budget} style={{size: 10}} onChange={(event) => setBudget(event.target.value)}/>
+			</span>
 		</div>
 	);
 };
